da19ac168c10118e35742b224df5e19b8c820240
Evan F <zenevan@gmail.com>
2026-04-23 04:20:57 -0500

    New Train Update
    working these out

---
+++ trains.lua
@@ -0,0 +1,329 @@
+-------------------------------------------------
+--         Put your Lua functions here.        --
+--                                             --
+-- Note that you can also use external Scripts --
+-------------------------------------------------
+-- TODO: update inittrain path in Silverymoon
+
+  testtrain = {
+    {48518, "Shalyssa Lurialar"},
+    {48850, "Forell Luckaun"},
+    {48721, "Seipora Rein"}
+  }
+
+  icecrag = {
+    {5804, "commander"},
+    {5735, "lieutenant"},
+    {5739, "lieutenant"},
+    {5769, "lieutenant"},
+    {5684, "lieutenant"},
+    {5833, "lieutenant"},
+    {5842, "lieutenant"},
+    {5780, "lieutenant"},
+    {5850, "lieutenant"},
+    {5752, "artist"},
+    {5772, "sentinel"},
+    {5748, "sentinel"}
+  }
+  smtrain = {
+    {84203, "bankDeposit"},
+    {48882, "Willa O'Greensleeves"},
+    {48873, "Aratha Sul"},
+    {48524, "Lavis Ryl"},
+    {48525, "Elaith Waterstill"},
+    {48518, "Shalyssa Lurialar"},
+    {48850, "Forell Luckaun"},
+    {48721, "Seipora Rein"},
+    {48681, "Corland Deepforge"},
+    {48586, "Ersenas"},
+    {49135, "Ultrumm"},
+--    {49054, "Tathshandra Tyrar"},
+--    {94648, "Otasit"},
+    {49117, "devout priest"},
+    {49036, "Dragor Keenblade"}
+  }
+      ashtrain = {
+    -- whore house
+    {87276, "naked woman"},
+    {87263, "gambler"},
+    {87276, "naked woman"},
+    {87272, "prostitute"},
+    {87262, "gambler"},
+    {87275, "aging prostitute"},
+    {87272, "prostitute"},
+    {87263, "gambler"},
+    {87276, "naked woman"},
+    {87263, "gambler"},
+    {87276, "naked woman"},
+    {87262, "gambler"},
+    {87276, "naked woman"},
+    {87272, "prostitute"},
+    {87275, "aging prostitute"},
+    {87272, "prostitute"},
+    {87275, "aging prostitute"},
+    {87276, "naked woman"},
+    {87263, "gambler"},
+    {87276, "naked woman"},
+    {87262, "gambler"},
+    {87272, "prostitute"},
+    {87275, "aging prostitute"},
+    {87272, "prostitute"},
+    {87263, "gambler"},
+    {87272, "prostitute"},
+    {87262, "gambler"},
+    {87275, "aging prostitute"},
+    {87263, "gambler"},
+    {87276, "naked woman"},
+    {87262, "gambler"},
+    -- nobles
+    {87258, "Ashstone noble"},
+    {87249, "Ashstone noble"},
+    {87258, "Ashstone noble"},
+    {87249, "Ashstone noble"},
+    {87251, "Ashstone noble"},
+    {87255, "Ashstone noble"},
+    {87251, "Ashstone noble"},
+    {87255, "Ashstone noble"},
+    {87253, "Ashstone noble"},
+    {87254, "Ashstone noble"},
+    {87257, "Ashstone noble"},
+    {87254, "Ashstone noble"},
+    {87257, "Ashstone noble"},
+    {87254, "Ashstone noble"},
+    {87257, "Ashstone noble"},
+    {87253, "Ashstone noble"},
+    {87255, "Ashstone noble"},
+    {87253, "Ashstone noble"},
+    {87255, "Ashstone noble"},
+    {87253, "Ashstone noble"},
+    {87255, "Ashstone noble"},
+    {87257, "Ashstone noble"},
+    {87254, "Ashstone noble"},
+    {87257, "Ashstone noble"},
+    {87253, "Ashstone noble"},
+    {87255, "Ashstone noble"},
+    {87253, "Ashstone noble"},
+    {87255, "Ashstone noble"},
+    {87251, "Ashstone noble"},
+    {87249, "Ashstone noble"},
+    {87251, "Ashstone noble"},
+    {87258, "Ashstone noble"},
+    {87249, "Ashstone noble"},
+    {87258, "Ashstone noble"},
+    -- deposit funds, send warning
+    {87415, "bankDeposit"}
+  cmtrain = {
+   -- {13402, "soldier"}, -- 25
+--    {85354, "captain"}, -- 30
+--    {85358, "leader"}, -- 30
+--    {85356, "elite guard"},
+--    {85410, "elite guard"},
+--    {85421, "elite guard"},
+-- {22222, soldier""}
+    {44315, "orc"},
+    {44317, "orc"},
+    {44314, "orc"},
+    {44317, "orc"},
+    {44318, "orc"},
+    {44319, "orc"},
+    {44320, "orc"},
+    {44321, "orc"},
+    {44324, "orc"},
+    {44325, "orc"},
+    {44326, "orc"},
+    {44322, "orc"},
+    {44333, "bugbear"},
+    {44335, "bugbear"},
+    {44336, "troll"},
+    {44338, "bugbear"},
+    {44343, "bugbear"},
+    {44334, "bugbear"},
+    {44332, "bugbear"},
+    {44344, "bugbear"},
+    {44346, "bugbear"},
+    {44314, "orc"}
+  }
+  wdtrain = {
+   -- {13402, "soldier"}, -- 25
+--    {85354, "captain"}, -- 30
+--    {85358, "leader"}, -- 30
+--    {85356, "elite guard"},
+--    {85410, "elite guard"},
+--    {85421, "elite guard"},
+   -- {22222, soldier""}
+    {74746, "orc"},
+    {74746, "soldier"}  
+  }
+  
+  
+  
+  
+  dktrain = {
+    -- The Purple Rothe Commons Room
+    {74687, "sage"},  -- 45
+    {74696, "leader"}, -- leader at 20+
+    {74687, "fat"},  -- 28
+    -- Before the House Zauviir Slave Pen
+    {74696, "soldier"},  -- singles
+    {74698, "bankDeposit"},
+   -- The House Hlaund Barracks  -- 74696
+    {74696, "soldier"}, -- soldier at 15
+    -- Before the House Hlaund Slave Pen
+    {74708, "soldier"}, -- singles
+    {74714, "leader"}, -- singles leader at 20+
+    {74687, "enigmatic"},  -- 45
+    {74696, "soldier"}, -- soldier at 15
+-- The House Rilynt'Tar Barracks  -- 74714
+    {74714, "soldier"}, -- soldier at 15
+    -- The Library of the Lost
+    {74723, "archivist"}, -- 45
+    {74733, "archer"}, --15
+    {74687, "emissary"},  -- 32
+    {74714, "soldier"}, -- soldier at 15
+    -- Before the House Rilynt'Tar Slave Pen
+    {74731, "leader"}, -- singles leader at 20+
+    {74725, "soldier"}, -- singles
+    {74687, "diplomat"},  -- 35
+    -- The House Hlaund Barracks  -- 74696
+    {74731, "soldier"}, -- soldier at 15
+    {74733, "archer"}, --15
+    {74731, "soldier"}, -- soldier at 15
+    -- Archers
+    {74733, "archer"}, --15
+    -- Bar
+    {74738, "warrior"},
+    {74733, "archer"}, --15
+    {74738, "hunter"},
+    {74733, "archer"}, --15
+    {74738, "smuggler"},
+    {74740, "soldier"}, -- singles
+    {74733, "captain"}, --captain at 25
+   -- The House Zauviir Barracks -- 74696
+    {74746, "leader"}, -- leader at 20+
+    {74855,"chef"}, -- 30
+    {74862,"male"}, -- 35    
+    {74746, "soldier"} -- soldier at 15
+    -- Before the House Hlaund Slave Pen
+}
+
+wgtrain = {
+--{442, "high"},--10
+--{436, "sister"},--11
+--{928, "lord"},--7
+--{999, "vigilant"}, ---9
+--{842, "syndar"},--10
+--{932, "priestess"}, --7
+--{441, "master"},----14
+--{511, "mistress"},--7
+--{437, "hunn"},  --13
+--{523, "high"},--10
+{327, "Inn"},
+{446, "abbot"},--13                                                                                                                              
+{473, "caressor"},--12--
+{474, "xalass"},--19
+{512, "kismet"},--19
+{524, "sunrise"},--19
+{614, "rider"},--12
+{615, "axil"},--19
+{843, "holycoin"},--19
+{929, "adeena"}, --19
+{933, "hond"},--19
+{959, "lidia"}, --19
+{1000, "grigor"},--19
+{1151, "dread"},--19
+{1158, "elort"},--
+{1151, "dread"},--19
+{1000, "grigor"},--19
+{959, "lidia"}, --19
+{933, "hond"},--19
+{929, "adeena"}, --19
+{843, "holycoin"},--19
+{615, "axil"},--19
+{614, "rider"},--12
+{524, "sunrise"},--19
+{512, "kismet"},--19
+{474, "xalass"},--19
+{473, "caressor"},--12--
+{446, "abbot"},--13                                                                                                                              
+{825, "BankDeposit"}
+}
+  --
+  raventrain = {
+    {85870, "captain"},
+    {85868, "reaver"}, 
+    {85879, "slaver"},
+    {85890, "captain"},
+    {85875, "reaver"}, 
+    {85889, "officer"},
+    {85878, "reaver"},
+    {85885, "knight"},
+    {85871, "reaver"},
+    {85886, "navigator"}
+  }
+
+  ghtrain = {
+    {41974, "young"},
+    {41975, "large"},
+    {41979, "young"},
+    {41964, "young"},
+    {41974, "large"},
+    {41979, "large"},
+    {41966, "young"},
+    {41973, "large"},
+    {41980, "young"},
+    {41979, "young"},
+    {41975, "large"},
+    {41964, "young"},
+    {41979, "large"},
+    {41965, "large"},
+    {41978, "young"},
+    {41966, "large"},
+    {41978, "large"},
+    {41975, "young"},
+    {41964, "large"},
+    {41965, "young"},
+    {41966, "large"},
+    {41973, "young"},
+    {41980, "large"},
+    {41975, "young"},
+    {41964, "large"},
+    {41965, "young"},
+    {41978, "large"},
+    {41979, "large"}
+  }   
+
+function configureTrain()
+  fwalkQue=false
+
+  -- disable any active triggers prior to enabling
+  expandAlias("@resettrain", false)
+
+  cecho("<green>[Laying train tracks...]\n")
+
+  if trainList ~= nil then
+    if _G[trainList] ~= nil then
+      NyyLIB.smtrainstops = _G[trainList]
+      trainList=nil
+      return
+    end
+  end
+
+  if map:getCurrentZone() == "Ashstone" then
+    NyyLIB.smtrainstops = ashtrain
+  elseif map:getCurrentZone() == "Silverymoon, Gem of the North" then
+    NyyLIB.smtrainstops = smtrain
+  elseif map:getCurrentZone() == "The Spirit Raven" then
+    NyyLIB.smtrainstops = raventrain
+  elseif map:getCurrentZone() == "Northern Waterdeep Main City" then
+    NyyLIB.smtrainstops = wdtrain
+  elseif map:getCurrentZone() == "Dobluth Kyor - Main City" then
+    NyyLIB.smtrainstops = dktrain
+  elseif map:getCurrentZone() == "Westgate" then
+    NyyLIB.smtrainstops = wgtrain
+  elseif map:getCurrentZone() == "The Comarian Mines" then
+    NyyLIB.smtrainstops = cmtrain
+  elseif map:getCurrentZone() == "Ghore - City of Trolls" then
+    NyyLIB.smtrainstops = ghtrain
+       
+  end
+end
